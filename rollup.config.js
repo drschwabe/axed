aa33a6f9a0fd05af5d3c10904129653664a17194
Derrick Schwabe <drschwabe@fastmail.com>
2022-08-25 11:34:18 -0600

    Remove unneeded rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,12 @@
-import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
-import json from '@rollup/plugin-json'
 
 export default {
   input: 'lib.js',
   output: {
     file: 'dist/lib.mjs',
-    format: 'esm',
+    format: 'es'
   },
   plugins: [
-    commonjs({
-      transformMixedEsModules : true,
-      esmExternals : true 
-    }), 
-    resolve(), json()]
+    commonjs(), 
+  ]
 }
